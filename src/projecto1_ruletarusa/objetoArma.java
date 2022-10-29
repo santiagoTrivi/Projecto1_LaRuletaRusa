55b4fddec929a68fc48e526b7c5ee54c9b5fcccc
santiagoTrivi <santiagocarvajal103@gmail.com>
2022-10-28 20:21:06 -0400

    2: creacion del objeto arma

---
+++ src/projecto1_ruletarusa/objetoArma.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projecto1_ruletarusa;
+
+/**
+ *
+ * @author Carvajal-Triviño
+ */
+public class objetoArma {
+    
+}
